3e8b432d4d18e4eb2c8ad36c884f6503d3048c04
valluvan-dev <btreerepo.pythonfsd@gmail.com>
2026-02-10 12:03:49 +0530

    chore: comment out development database and allowed hosts
    These settings are for development and should be configured via environment variables or a separate settings file for production. Commenting them out prevents accidental use of the SQLite database and overly permissive host settings in a deployed environment.

--- core/settings.py
+++ core/settings.py
@@ -213,14 +213,14 @@ DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 # # for developement
 
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
